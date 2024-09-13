c820393dee15176a23adc006bb6114c3ca0d45ea
hasankhairullahdev <hassan.khairullah@gmail.com>
2024-09-13 10:56:10 +0700

    Catalog Api Done,

---
+++ src/Services/Catalog/Catalog.API/GlobalUsing.cs
@@ -0,0 +1,12 @@
+﻿global using Carter;
+global using Mapster;
+global using MediatR;
+global using Marten;
+global using BuildingBlocks.CQRS;
+global using BuildingBlocks.Exceptions;
+global using BuildingBlocks.Exceptions.Handler;
+global using BuildingBlocks.Behaviors;
+global using Catalog.API.Models;
+global using Catalog.API.Data;
+global using Catalog.API.Exceptions;
+global using FluentValidation;
---
+++ src/Services/Catalog/Catalog.API/Products/GetProducts/GetProductsHandler.cs
@@ -0,0 +1,17 @@
+﻿using Marten.Linq.QueryHandlers;
+using Marten.Pagination;
+
+namespace Catalog.API.Products.GetProducts
+{
+    public record GetProductsQuery(int? PageNumber = 1, int? PageSize = 10) : IQuery<GetProductsResult>;
+    public record GetProductsResult(IEnumerable<Product> Products);
+    internal class GetProductsQueryHandler(IDocumentSession session) : IQueryHandler<GetProductsQuery, GetProductsResult>
+    {
+        public async Task<GetProductsResult> Handle(GetProductsQuery query, CancellationToken cancellationToken)
+        {
+            var product = await session.Query<Product>().ToPagedListAsync(query.PageNumber ?? 1, query.PageSize ?? 10, cancellationToken);
+
+            return new GetProductsResult(product);
+        }
+    }
+}
