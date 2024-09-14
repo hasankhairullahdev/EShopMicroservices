b288becca32a14d54df4328ccb28b6705be99a89
hasankhairullahdev <hassan.khairullah@gmail.com>
2024-09-14 14:14:29 +0700

    basket api. belum beres semua

---
+++ src/Services/Basket/Basket.API/Basket/DeleteBasket/DeleteBasketEndpoints.cs
@@ -0,0 +1,28 @@
+﻿
+using Basket.API.Basket.StoreBasket;
+
+namespace Basket.API.Basket.DeleteBasket
+{
+    //public record DeleteBasketRequest(string UserName)
+
+    public record DeleteBasketResponse(bool IsSuccess);
+    public class DeleteBasketEndpoints : ICarterModule
+    {
+        public void AddRoutes(IEndpointRouteBuilder app)
+        {
+            app.MapDelete("/basket/{userName}", async (string userName, ISender sender) =>
+            {
+                var result = await sender.Send(new DeleteBasketCommand(userName));
+
+                var response = result.Adapt<DeleteBasketResponse>();
+
+                return Results.Ok(response);
+            })
+            .WithName("DeleteBasket")
+            .Produces<StoreBasketResponse>(StatusCodes.Status201Created)
+            .ProducesProblem(StatusCodes.Status400BadRequest)
+            .WithSummary("Delete Basket")
+            .WithDescription("Delete Basket");
+        }
+    }
+}
